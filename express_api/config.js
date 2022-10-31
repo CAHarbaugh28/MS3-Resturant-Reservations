003d65049b563e720cfaaaf04e0ce6304ae5e3c5
Carter Harbaugh <carter.harbaugh@gmail.com>
2022-10-30 23:46:20 -0500

    remove unused dependencies

--- express_api/config.js
+++ express_api/config.js
@@ -1,34 +1,15 @@
+console.log(`Starting up...`)
 require('dotenv').config()
 
 //Native PG Configure
 const { Pool } = require('pg')
 const isProduction = process.env.NODE_ENV === 'production'
-const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
-
+const connectionString = `postgresql://${process.env.DB_USER || 'app_user'}:${process.env.DB_PASSWORD || 'some_password'}@${process.env.DB_HOST || '127.0.0.1' }:${process.env.DB_PORT || '5432' }/${process.env.DB_DATABASE || 'resturantreservations'}`
+console.log(`Environment: ${process.env.NODE_ENV}`)
+console.log(`Environment: ${process.env.DATABASE_URL}`)
 const pool = new Pool({
   connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
   ssl: isProduction,
 })
 
-//Sequalize Configure
-const Sequelize = require("sequelize");
-const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  dialect: process.env.DB_DIALECT,
-
-  pool: {
-    max: ~~process.env.POOL_MAX,
-    min: ~~process.env.POOL_MIN,
-    acquire: ~~process.env.POOL_AQUIRE,
-    idle: ~~process.env.POOL_IDLE
-  }
-});
-
-const seqDb = {};
-
-seqDb.Sequelize = Sequelize;
-seqDb.sequelize = sequelize;
-
-///seqDb.tutorials = require("./tutorial.model.js")(sequelize, Sequelize);
-
-module.exports = { pool, seqDb }
+module.exports = { pool }
