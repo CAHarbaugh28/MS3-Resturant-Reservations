d1c1b0d72f26e970e8b71c129d32b5f8038e6f94
Carter Harbaugh <carter.harbaugh@gmail.com>
2022-10-30 23:48:36 -0500

    Updated config.js logging of env settings

--- express_api/config.js
+++ express_api/config.js
@@ -6,7 +6,7 @@ const { Pool } = require('pg')
 const isProduction = process.env.NODE_ENV === 'production'
 const connectionString = `postgresql://${process.env.DB_USER || 'app_user'}:${process.env.DB_PASSWORD || 'some_password'}@${process.env.DB_HOST || '127.0.0.1' }:${process.env.DB_PORT || '5432' }/${process.env.DB_DATABASE || 'resturantreservations'}`
 console.log(`Environment: ${process.env.NODE_ENV}`)
-console.log(`Environment: ${process.env.DATABASE_URL}`)
+console.log(`Database Url: ${process.env.DATABASE_URL}`)
 const pool = new Pool({
   connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
   ssl: isProduction,
