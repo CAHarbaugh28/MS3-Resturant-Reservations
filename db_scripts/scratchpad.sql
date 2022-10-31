8b04ea10a2db66c10c7b14fc5817083115f6b4d9
Carter Harbaugh <carter.harbaugh@gmail.com>
2022-10-30 21:03:55 -0500

    Added Heroku profile

--- db_scripts/scratchpad.sql
+++ db_scripts/scratchpad.sql
@@ -42,8 +42,89 @@ values (1, 1, '2022-11-11', '12:00 PM', CURRENT_TIMESTAMP, false, false),
 (1, 1, '2022-11-11', '08:00 PM', CURRENT_TIMESTAMP, false, false),
 (1, 1, '2022-11-11', '09:00 PM', CURRENT_TIMESTAMP, false, false)
 
-delete from public.reservations where id = 4;
+delete from public.reservations where id = 12;
 
-INSERT INTO public.reservations
+insert into public.reservations
 (customer_id, table_id, r_date, r_time, created_date, arrived, cancelled)
-VALUES(2, 2, '2022-11-11', '07:00 PM', CURRENT_TIMESTAMP, false, false);
+values (2, 2, '2022-11-11', '09:00 PM', CURRENT_TIMESTAMP, false, false)
+
+
+create or replace procedure public.new_reservation(
+	customerId int,
+	tableId int,
+	rdate date,
+	rtime time, 
+	inout confirmationCode varchar(8)
+)
+language plpgsql    
+as $$
+begin
+	insert into public.reservations
+	(customer_id, table_id, r_date, r_time)
+	values (customerId, tableId, rdate, rtime)
+	on conflict on constraint const_no_duplicate_reservations do nothing;
+	
+	select confirmationId 
+	into confirmationCode 
+	from public.reservations r 
+	where customer_id = customerId
+		and table_id = tableId 
+		and r_date = rdate
+		and r_time = rtime;
+		
+    commit;
+end;$$
+
+
+DO
+$$
+DECLARE confirmationCode varchar(8);
+BEGIN
+CALL public.new_reservation(2,3, '2022-11-25','06:00:00 PM',confirmationCode);
+raise notice 'ConfirmationId: %', confirmationCode;
+end;
+$$;
+
+
+create or replace procedure public.upsert_customer(
+	fname varchar(20),
+	lname varchar(20),
+	phone varchar(10),
+	eaddy varchar,
+	inout customerId int
+)
+language plpgsql    
+as $$
+begin
+	insert into public.customer_info 
+		(first_name, last_name, phone_number, email)
+	values 
+		(fname, lname, phone, eaddy)
+	on conflict on constraint const_no_duplicate_customer do nothing;
+	
+	select id
+	into customerId
+	from public.customer_info r 
+	where first_name = fname 
+		and last_name = lname
+		and phone_number = phone
+		and email = eaddy
+	limit 1;
+
+    commit;
+end
+$$;
+
+
+DO
+$$
+DECLARE customerId INT;
+BEGIN
+CALL public.upsert_customer('Phil','Jackson','8164059774','chdev77@gmail.com',customerId);
+raise notice 'Value: %', customerId;
+end;
+$$;
+
+select * from public.customer_info ci 
+
+
