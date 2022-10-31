b8b98441a888b5d1b16702c51c91611ce03cb451
Carter Harbaugh <carter.harbaugh@gmail.com>
2022-10-31 00:45:29 -0500

    heroku - fix config.js ssl flag for pg access

--- express_api/config.js
+++ express_api/config.js
@@ -7,9 +7,11 @@ const isProduction = process.env.NODE_ENV === 'production'
 const connectionString = `postgresql://${process.env.DB_USER || 'app_user'}:${process.env.DB_PASSWORD || 'some_password'}@${process.env.DB_HOST || '127.0.0.1' }:${process.env.DB_PORT || '5432' }/${process.env.DB_DATABASE || 'resturantreservations'}`
 console.log(`Environment: ${process.env.NODE_ENV}`)
 console.log(`Database Url: ${process.env.DATABASE_URL}`)
+
+//https://stackoverflow.com/questions/62098110/heroku-app-throwing-error-self-signed-certificate
 const pool = new Pool({
   connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
-  ssl: isProduction,
+  ssl: { rejectUnauthorized: false },
 })
 
 module.exports = { pool }
