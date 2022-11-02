af15a81bccd5ed01f73a29e555e194c01bbff0d3
Carter Harbaugh <carter.harbaugh@gmail.com>
2022-11-02 00:51:00 -0500

    Example on how to call getAvailibleTimes

--- client/src/views/places/Reserve.jsx
+++ client/src/views/places/Reserve.jsx
@@ -14,21 +14,39 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
 import BasicModal from '../../components/reserve';
 
-
-
 export default function Reserve() {
     const [open, setOpen] = React.useState(false);
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
 
-    const [age, setAge] = React.useState('');
-    const [date, setValue] = React.useState(dayjs());
-    const setTableSelection = (event) => {
-      setAge(event.target.value);
+
+    const [currentSelectedTableSate, setSelectedTableState] = React.useState('');
+    const [currentReservationDateState, setReservationDateState] = React.useState('');
+    const [currentAvailableTimesFromServer, setAvailableTimesFromServer] = React.useState([]);
+    const [formattedTableColRowArraryForApi, setFormattedTableColRowArraryForApi] = React.useState([]);
+    const [formateedDateForApi, setFormateedDateForApi] = React.useState('');
+
+    const setSelectedTable = (value) => {
+      setSelectedTableState(value.target.value);
+
+      setFormattedTableColRowArraryForApi(value.target.value.split("/")); //we need to split here because the need passed into the api as row & col separatly parameters
     };
 
-    const setReservationDate = (newValue) => {
-      setValue(newValue);
+    const setReservationDate = (value) => {
+      setReservationDateState(value);
+
+      setFormateedDateForApi(`${value.$y}-${value.$M}-${value.$D}`);
+      var serverResposneTimes = getAvailableTimes(formattedTableColRowArraryForApi[0], formattedTableColRowArraryForApi[1], `${value.$y}-${value.$M}-${value.$D}`);
+      setAvailableTimesFromServer(serverResposneTimes);
+    };
+
+    const getAvailableTimes = (trow, tcol, rdate) => {
+      fetch(`/api/table/getAvailableTimes/${trow}/${tcol}/${rdate}`)
+        .then(result => result.json())
+        .then(body => {
+          console.log(body);
+          setAvailableTimesFromServer(body);
+        });
     };
 
   return (
@@ -40,7 +58,7 @@ export default function Reserve() {
     alignItems="center"
     textAlign="center"
     justifyContent="center"
-    colro>
+    >
     <Paper>
         <div className="reservePage"/>
             <div className="tableSelect">
@@ -50,28 +68,28 @@ export default function Reserve() {
                 <Select
                 labelId="demo-simple-select-label"
                 id="demo-simple-select"
-                value={age}
-                label="Age"
-                onChange={setTableSelection}
+                value={currentSelectedTableSate}
+                label="Table"
+                onChange={setSelectedTable}
                 >
-                    <MenuItem value={1}>1A</MenuItem>
-                    <MenuItem value={2}>1B</MenuItem>
-                    <MenuItem value={3}>1C</MenuItem>
-                    <MenuItem value={4}>1D</MenuItem>
-                    <MenuItem value={5}>2A</MenuItem>
-                    <MenuItem value={6}>2B</MenuItem>
-                    <MenuItem value={7}>2C</MenuItem>
-                    <MenuItem value={8}>3A</MenuItem>
-                    <MenuItem value={9}>3B</MenuItem>
-                    <MenuItem value={10}>3C</MenuItem>
-                    <MenuItem value={11}>4A</MenuItem>
+                    <MenuItem value={'1/A'}>1A</MenuItem>
+                    <MenuItem value={'1/B'}>1B</MenuItem>
+                    <MenuItem value={'1/C'}>1C</MenuItem>
+                    <MenuItem value={'1/D'}>1D</MenuItem>
+                    <MenuItem value={'2/A'}>2A</MenuItem>
+                    <MenuItem value={'2/B'}>2B</MenuItem>
+                    <MenuItem value={'2/C'}>2C</MenuItem>
+                    <MenuItem value={'3/A'}>3A</MenuItem>
+                    <MenuItem value={'4/A'}>4A</MenuItem>
+                    <MenuItem value={'4/B'}>4B</MenuItem>
+                    <MenuItem value={'4/C'}>4C</MenuItem>
                 </Select>
             </FormControl>
             <LocalizationProvider dateAdapter={AdapterDayjs}>
                 <DesktopDatePicker
-                label="Date desktop"
+                label="Reservation Date"
                 inputFormat="MM/DD/YYYY"
-                value={date}
+                value={currentReservationDateState}
                 onChange={setReservationDate}
                 renderInput={(params) => <TextField {...params} />}
                 />
